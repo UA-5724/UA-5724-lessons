4d75cd84b838f158335f6745814c7f4eb840aafb
olmk9091 <olmk9091@gmail.com>
2026-06-18 14:12:23 +0300

    feat: додано скріншоти виконаного ДЗ02 від olmk9091

--- hw/hw02/olmk9091/Console Application/Console Application/Program.cs
+++ hw/hw02/olmk9091/Console Application/Console Application/Program.cs
@@ -20,77 +20,78 @@ class Program
         {
             Console.WriteLine("Cannot divide by zero");
         }
-        ////2 Simple Question
 
-        ////write a question
-        //Console.WriteLine("How are you?");
-        ////read user's answer
-        //string answer = Console.ReadLine()!;
-        ////show result
-        //Console.WriteLine("You are " + answer);
+        //2 Simple Question
 
-        ////3. Working with char
+        //write a question
+        Console.WriteLine("How are you?");
+        //read user's answer
+        string answer = Console.ReadLine()!;
+        //show result
+        Console.WriteLine("You are " + answer);
 
-        ////after user type string(text) convert text into a single character, store in the variable first, second, third
-        //char first = char.Parse(Console.ReadLine()!);
-        //char second = char.Parse(Console.ReadLine()!);
-        //char third = char.Parse(Console.ReadLine()!);
-        ////displays the values stored in the variables
-        //Console.WriteLine("You entered " + first + ", " + second + ", " + third);
+        //3. Working with char
 
-        ////4. Boolean Expression
+        //after user type string(text) convert text into a single character, store in the variable first, second, third
+        char first = char.Parse(Console.ReadLine()!);
+        char second = char.Parse(Console.ReadLine()!);
+        char third = char.Parse(Console.ReadLine()!);
+        //displays the values stored in the variables
+        Console.WriteLine("You entered " + first + ", " + second + ", " + third);
 
-        ////read first number
-        //int c = int.Parse(Console.ReadLine()!);
-        ////read second number
-        //int d = int.Parse(Console.ReadLine()!);
-        ////verify conditions
-        //bool bothPositive = c > 0 && d > 0;
-        ////show results
-        //Console.WriteLine(bothPositive);
+        //4. Boolean Expression
 
-        ////5. Square Calculations
+        //read first number
+        int c = int.Parse(Console.ReadLine()!);
+        //read second number
+        int d = int.Parse(Console.ReadLine()!);
+        //verify conditions
+        bool bothPositive = c > 0 && d > 0;
+        //show results
+        Console.WriteLine(bothPositive);
 
-        ////read entered data
-        //int e = int.Parse(Console.ReadLine()!);
-        ////calculate area
-        //int area = e * e;
-        ////calculate perimeter
-        //int perimeter = 4 * e;
-        ////show results
-        //Console.WriteLine(area);
-        //Console.WriteLine(perimeter);
+        //5. Square Calculations
 
-        ////6. Name and Age Interaction
+        //read entered data
+        int e = int.Parse(Console.ReadLine()!);
+        //calculate area
+        int area = e * e;
+        //calculate perimeter
+        int perimeter = 4 * e;
+        //show results
+        Console.WriteLine(area);
+        Console.WriteLine(perimeter);
 
-        ////declare a value
-        //string name;
-        //int age;
-        //// show the question
-        //Console.WriteLine("What is your name?");
-        ////read the name
-        //name = Console.ReadLine()!;
-        //// show the question
-        //Console.WriteLine("How old are you, " + name + "?");
-        ////read the age
-        //age = int.Parse(Console.ReadLine()!);
-        ////show results
-        //Console.WriteLine("Name: " + name);
-        //Console.WriteLine("Age: " + age);
+        //6. Name and Age Interaction
 
-        ////7. Circle Calculations
+        //declare a value
+        string name;
+        int age;
+        // show the question
+        Console.WriteLine("What is your name?");
+        //read the name
+        name = Console.ReadLine()!;
+        // show the question
+        Console.WriteLine("How old are you, " + name + "?");
+        //read the age
+        age = int.Parse(Console.ReadLine()!);
+        //show results
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
 
-        ////read the entered data about radius
-        //double r = double.Parse(Console.ReadLine()!);
-        ////calculate the circle length
-        //double l = 2 * Math.PI * r;
-        ////calculate the circle area
-        //double s = Math.PI * r * r;
-        ////calculate ball area
-        //double v = 4.0 / 3 * Math.PI * r * r * r;
-        ////show results
-        //Console.WriteLine(l);
-        //Console.WriteLine(s);
-        //Console.WriteLine(v);
+        //7. Circle Calculations
+
+        //read the entered data about radius
+        double r = double.Parse(Console.ReadLine()!);
+        //calculate the circle length
+        double l = 2 * Math.PI * r;
+        //calculate the circle area
+        double s = Math.PI * r * r;
+        //calculate ball area
+        double v = 4.0 / 3 * Math.PI * r * r * r;
+        //show results
+        Console.WriteLine(l);
+        Console.WriteLine(s);
+        Console.WriteLine(v);
     }
 }

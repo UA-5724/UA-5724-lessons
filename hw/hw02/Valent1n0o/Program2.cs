72d11b89797f25c451a1d688b5a7d51e1964e4af
Valentyn Muradov <valentino.muradov@gmail.com>
2026-06-24 18:28:15 +0300

    hw02 Valent1n0o

---
+++ hw/hw02/Valent1n0o/Program2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hw2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+
+            Console.WriteLine("How are you?");
+
+            answer = Console.ReadLine();
+
+            Console.WriteLine("You are " + answer);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ hw/hw02/Valent1n0o/Program6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace hw2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"How old are you, {name}?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+
+            Console.ReadKey();
+        }
+    }
+}

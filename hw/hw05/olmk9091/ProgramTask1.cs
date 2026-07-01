b67f96561a4cc05c6cb102092e257bd19b6e1539
olmk9091 <olmk9091@gmail.com>
2026-07-01 10:58:08 +0300

    feat: додано код виконаного ДЗ05 від olmk9091

---
+++ hw/hw05/olmk9091/ProgramTask1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+class Car
+{
+    private string name = "";
+    private string color = "";
+    private double price;
+    //company Name for all cars
+    private const string CompanyName = "BMW";
+    //default constructor
+    public Car() 
+    {
+    }
+    //constructor with parameters
+    public Car(string name, string color, double price)
+    {
+    //Initialize object fields
+    this.name = name;
+    this.color = color;
+    this.price = price;
+    }
+    //property for the car color
+    public string Color
+    {
+        get
+        {
+            //return the current color
+            return color;
+        }
+
+        set
+        {
+            //check that the value is not null or empty
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                //set a new color
+                color = value;
+            }
+        }
+    }
+    public void Input()
+    {
+        //read car data from console
+        name = Console.ReadLine()!;
+        color = Console.ReadLine()!;
+        price = double.Parse(Console.ReadLine()!);
+    }
+    //display car info
+    public void Print()
+    {
+        Console.WriteLine(ToString());
+    }
+    //increase the car price by the given %
+    public void ChangePrice(double x)
+    {
+        //change the car price by x %
+        price = price + price * x / 100;
+    }
+    //compare two cars by name and price
+    public static bool operator ==(Car a, Car b)
+    {
+        //varify null
+        if (ReferenceEquals(a, b))
+        {
+            return true;
+        }
+
+        if (a is null || b is null)
+        {
+            return false;
+        }
+        return a.name == b.name &&
+               a.price == b.price;
+    }
+    //compare two cars for inequality
+    public static bool operator !=(Car a, Car b)
+    {
+        return !(a == b);
+    }
+    //return formatted car info
+    public override string ToString()
+    {
+        return $"Company: {CompanyName}, Name: {name}, Color: {color}, Price: {price}";
+    }
+    //compare this object with another object
+    public override bool Equals(object? obj)
+    {
+        if (obj is Car other)
+        {
+            return name == other.name &&
+                   price == other.price;
+        }
+
+        return false;
+    }
+    //return a hash code based on the car info
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(name, price);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        //create three car objects
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+
+        //read info for each car
+        car1.Input();
+        car2.Input();
+        car3.Input();
+
+        //decrease the price of each car by 10 %
+        car1.ChangePrice(-10);
+        car2.ChangePrice(-10);
+        car3.ChangePrice(-10);
+
+        //show updated info
+        car1.Print();
+        car2.Print();
+        car3.Print();
+
+       //read new color
+        string newColor = Console.ReadLine()!;
+        //repaint white cars, take into account all cases (White, white, WHITE, WhItE)
+        if (car1.Color.Equals("White", StringComparison.OrdinalIgnoreCase))
+        {
+                car1.Color = newColor;
+            }
+        if (car2.Color.Equals("White", StringComparison.OrdinalIgnoreCase))
+        {
+            car2.Color = newColor;
+        }
+        if (car3.Color.Equals("White", StringComparison.OrdinalIgnoreCase))
+        {
+            car3.Color = newColor;
+        }
+        //show cars using ToString()
+        Console.WriteLine(car1);
+        Console.WriteLine(car2);
+        Console.WriteLine(car3);
+    }
+}
---
+++ hw/hw05/olmk9091/ProgramTask2.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections;
+class Person
+{
+    //person fields
+    private string name = "";
+    private DateTime birthYear;
+    public Person() 
+    {
+    }
+    //constructor with parameters
+    public Person(string name, DateTime birthYear)
+    {
+    //Initialize object fields
+    this.name = name;
+    this.birthYear = birthYear;
+    }
+    public string Name
+    {
+        get
+        {
+            //return the current name
+            return name;
+        }
+    }
+    public DateTime BirthYear
+    {
+        get
+        {
+            //return the person's birth date
+            return birthYear;
+        }
+    }
+    //сalculate the person's age
+    public int Age()
+    {
+        return DateTime.Now.Year - birthYear.Year;
+    }
+    public void Input()
+    {
+        //read person data from console
+        name = Console.ReadLine()!;
+        birthYear = DateTime.Parse(Console.ReadLine()!);
+    }
+    //Change the person's name
+    public void ChangeName(string newName)
+    {
+        name = newName;
+    }
+    //compare names
+    public static bool operator ==(Person a, Person b)
+    {
+        if (ReferenceEquals(a, b))
+        {  
+            return true;
+        }
+
+        if (a is null || b is null)
+        { 
+            return false; 
+        }
+
+        return a.name == b.name;
+    }
+    public static bool operator !=(Person a, Person b)
+    {
+        return !(a == b);
+    }
+    //return formatted person info
+    public override string ToString()
+    {
+        return $"Name: {name}, Age: {Age ()}";
+    }
+    //сompare two persons by name
+    public override bool Equals(object? obj)
+    {
+        if (obj is Person other)
+        {
+            return name == other.name;
+        }
+
+        return false;
+    }
+    //return hash code based on the person's name
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(name);
+    }
+    //display person info
+    public void Output()
+    {
+        Console.WriteLine(ToString());
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        //create 6 person objects
+        Person p1 = new Person();
+        Person p2 = new Person();
+        Person p3 = new Person();
+        Person p4 = new Person();
+        Person p5 = new Person();
+        Person p6 = new Person();
+
+        //read info for each person
+        p1.Input();
+        p2.Input();
+        p3.Input();
+        p4.Input();
+        p5.Input();
+        p6.Input();
+        //rename person younger than 16
+        if (p1.Age() < 16)
+        {
+            p1.ChangeName("Very Young");
+        }
+        if (p2.Age() < 16)
+        {
+            p2.ChangeName("Very Young");
+        }
+        if (p3.Age() < 16)
+        {
+            p3.ChangeName("Very Young");
+        }
+        if (p4.Age() < 16)
+        {
+            p4.ChangeName("Very Young");
+        }
+        if (p5.Age() < 16)
+        {
+            p5.ChangeName("Very Young");
+        }
+        if (p6.Age() < 16)
+        {
+            p6.ChangeName("Very Young");
+        }
+        //find persons with same name
+        if (p1 == p2)
+        {
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+        if (p1 == p3)
+        {
+            Console.WriteLine(p1);
+            Console.WriteLine(p3);
+        }
+        if (p1 == p4)
+        {
+            Console.WriteLine(p1);
+            Console.WriteLine(p4);
+        }
+        if (p1 == p5)
+        {
+            Console.WriteLine(p1);
+            Console.WriteLine(p5);
+        }
+        if (p1 == p6)
+        {
+            Console.WriteLine(p1);
+            Console.WriteLine(p6);
+        }
+        if (p2 == p3)
+        {
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+        }
+        if (p2 == p4)
+        {
+            Console.WriteLine(p2);
+            Console.WriteLine(p4);
+        }
+        if (p2 == p5)
+        {
+            Console.WriteLine(p2);
+            Console.WriteLine(p5);
+        }
+        if (p2 == p6)
+        {
+            Console.WriteLine(p2);
+            Console.WriteLine(p6);
+        }
+        if (p3 == p4)
+        {
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
+        }
+        if (p3 == p5)
+        {
+            Console.WriteLine(p3);
+            Console.WriteLine(p5);
+        }
+        if (p3 == p6)
+        {
+            Console.WriteLine(p3);
+            Console.WriteLine(p6);
+        }
+        if (p4 == p5)
+        {
+            Console.WriteLine(p4);
+            Console.WriteLine(p5);
+        }
+        if (p4 == p6)
+        {
+            Console.WriteLine(p4);
+            Console.WriteLine(p6);
+        }
+        if (p5 == p6)
+        {
+            Console.WriteLine(p5);
+            Console.WriteLine(p6);
+        }
+        //show person info
+        p1.Output();
+        p2.Output();
+        p3.Output();
+        p4.Output();
+        p5.Output();
+        p6.Output();
+    }
+}

f83ae2cfdf6213ac291743e995850e45539a40ba
BohdanaVed <bvedm@softserveinc.com>
2026-08-12 22:55:55 +0300

    Added hw08

---
+++ hw/hw08/BohdanaVed/DivisionByZeroException.cs
@@ -0,0 +1,9 @@
+namespace HW8
+{
+    internal class DivisionByZeroException : Exception
+    {
+        public DivisionByZeroException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ hw/hw08/BohdanaVed/NumberOutOfRangeException.cs
@@ -0,0 +1,9 @@
+namespace HW8
+{
+    internal class NumberOutOfRangeException : Exception
+    {
+        public NumberOutOfRangeException(string message) : base(message)
+        {
+        }
+    }
+}
